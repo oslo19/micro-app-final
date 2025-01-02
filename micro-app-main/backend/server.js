879675ef4709c99968789d7111cd1f106cf6b7a8
Genard <119303896+oslo19@users.noreply.github.com>
2025-01-02 21:18:27 +0800

    Update server.js

--- micro-app-main/backend/server.js
+++ micro-app-main/backend/server.js
@@ -20,9 +20,16 @@ app.use(cors({
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
-  credentials: true,
-  maxAge: 86400 // 24 hours
+  credentials: true
 }));
+
+// Add security headers
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Accept');
+  next();
+});
 
 app.use(express.json());
 
